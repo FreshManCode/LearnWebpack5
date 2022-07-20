fc5a7dfd7c76f1781684c4f26cf908169074c3ba
zhangjunjun <649283213@qq.com>
2022-07-20 10:18:20 +0800

    webpack5 高级应用学习

--- 02-step-app/10-multiple-env/config/webpack.config.js
+++ 02-step-app/10-multiple-env/config/webpack.config.js
@@ -4,6 +4,7 @@ const commonConfig = require('./webpack.config.common')
 const productionConfig  = require('./webpack.config.prod')
 const developmentConfig = require('./webpack.config.dev')
 
+// 通过启动脚本中的 --env 参数来设置相关参数
 module.exports = (env)=>{
     console.log('env is:',env);
     switch (true) {
